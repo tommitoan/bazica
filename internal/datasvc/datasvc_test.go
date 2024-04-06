a2d126f725480eabd425d18e725cfbaa697767df
tommitoan <htm1995@gmail.com>
2024-04-07 00:06:41 +0700

    test: remove main.go

--- internal/datasvc/datasvc_test.go
+++ internal/datasvc/datasvc_test.go
@@ -1,31 +1,27 @@
 package datasvc
 
-import (
-	"testing"
-)
-
-func TestGetSolarTermsByYear(t *testing.T) {
-
-	tests := []struct {
-		name        string
-		year        string
-		expectedErr error
-	}{
-		// TODO: Add test cases.
-		{
-			name:        "random",
-			year:        "1900",
-			expectedErr: nil,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			_, gotErr := GetSolarTermsByYear("solar-terms-data/", tt.year)
-			if gotErr != tt.expectedErr {
-				t.Errorf("%s: got = %v, expect = %v", tt.name, gotErr, tt.expectedErr)
-				return
-			}
-
-		})
-	}
-}
+//func TestGetSolarTermsByYear(t *testing.T) {
+//
+//	tests := []struct {
+//		name        string
+//		year        string
+//		expectedErr error
+//	}{
+//		// TODO: Add test cases.
+//		{
+//			name:        "random",
+//			year:        "1900",
+//			expectedErr: nil,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			_, gotErr := GetSolarTermsByYearV2("solar-terms-data/", tt.year)
+//			if gotErr != tt.expectedErr {
+//				t.Errorf("%s: got = %v, expect = %v", tt.name, gotErr, tt.expectedErr)
+//				return
+//			}
+//
+//		})
+//	}
+//}
