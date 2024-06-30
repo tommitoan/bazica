dc579c873975d768ae6e856f108ec184da62aa6e
tommitoan <htm1995@gmail.com>
2024-06-30 23:24:22 +0700

    feat: detect solar term by input time

---
+++ model.go
@@ -0,0 +1,35 @@
+package bazica
+
+type SolarTermYear struct {
+	MinorCold          string `json:"minor_cold"`
+	MajorCold          string `json:"major_cold"`
+	StartOfSpring      string `json:"start_of_spring"`
+	SpringShowers      string `json:"spring_showers"`
+	AwakeningOfInsects string `json:"awakening_of_insects"`
+	SpringEquinox      string `json:"spring_equinox"`
+	PureBrightness     string `json:"pure_brightness"`
+	GrainRain          string `json:"grain_rain"`
+	StartOfSummer      string `json:"start_of_summer"`
+	GrainBuds          string `json:"grain_buds"`
+	GrainInEar         string `json:"grain_in_ear"`
+	SummerSolstice     string `json:"summer_solstice"`
+	MinorHeat          string `json:"minor_heat"`
+	MajorHeat          string `json:"major_heat"`
+	StartOfAutumn      string `json:"start_of_autumn"`
+	EndOfHeat          string `json:"end_of_heat"`
+	WhiteDew           string `json:"white_dew"`
+	AutumnEquinox      string `json:"autumn_equinox"`
+	ColdDew            string `json:"cold_dew"`
+	Frost              string `json:"frost"`
+	StartOfWinter      string `json:"start_of_winter"`
+	MinorSnow          string `json:"minor_snow"`
+	MajorSnow          string `json:"major_snow"`
+	WinterSolstice     string `json:"winter_solstice"`
+}
+
+type CombinedData struct {
+	Year string        `json:"year"` // Year extracted from filename
+	Data SolarTermYear `json:"data"` // Data for the specific year
+}
+
+var combinedData map[string]CombinedData
