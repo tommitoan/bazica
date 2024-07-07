11a174b83970d0afb55c86931b011a10657723e0
tommitoan <htm1995@gmail.com>
2024-07-07 10:04:42 +0700

    hotfix return bazi chart

--- internal/fourpillars/get_four_pillars.go
+++ internal/fourpillars/get_four_pillars.go
@@ -13,37 +13,37 @@ func GetFourPillars(dateTime time.Time, loc *time.Location, prefixPath ...string
 		path = prefixPath[0]
 	}
 
-	var baziChart model.FourPillars
+	var fourPillars model.FourPillars
 
 	// Get Year pillar
 	yearPillar, err := GetYearPillar(path, dateTime)
 	if err != nil {
 		return nil, err
 	}
-	baziChart.YearPillar = yearPillar
+	fourPillars.YearPillar = yearPillar
 
 	// Get Month pillar
 	monthPillar, err := GetMonthPillar(path, yearPillar, dateTime)
 	if err != nil {
 		return nil, err
 	}
-	baziChart.MonthPillar = monthPillar
+	fourPillars.MonthPillar = monthPillar
 
 	// Get Day pillar
 	dayPillar, err := GetDayPillar(dateTime, loc)
 	if err != nil {
 		return nil, err
 	}
-	baziChart.DayPillar = dayPillar
+	fourPillars.DayPillar = dayPillar
 
 	// Get Hour pillar
 	hourPillar, err := GetHourPillar(dayPillar, dateTime)
 	if err != nil {
 		return nil, err
 	}
-	baziChart.HourPillar = hourPillar
+	fourPillars.HourPillar = hourPillar
 
-	jsonData, _ := json.Marshal(baziChart)
+	jsonData, _ := json.Marshal(fourPillars)
 	slog.Info(string(jsonData))
-	return &baziChart, nil
+	return &fourPillars, nil
 }
